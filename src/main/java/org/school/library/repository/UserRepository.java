bd838d4097feaea3f541a2993357ee5445551e64
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2022-01-09 18:03:56 +0200

    ft(user): added repository

---
+++ src/main/java/org/school/library/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.school.library.repository;
+
+import org.school.library.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
