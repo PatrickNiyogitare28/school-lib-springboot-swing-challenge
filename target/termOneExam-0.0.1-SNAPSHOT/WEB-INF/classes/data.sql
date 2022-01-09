a1f9ab2a5b05fae6fb1d3e1eaefa067561377809
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2022-01-10 01:58:33 +0200

    chore: removed target

--- target/termOneExam-0.0.1-SNAPSHOT/WEB-INF/classes/data.sql
+++
@@ -1,4 +0,0 @@
-insert into item(id, name, weather) values(101,'Kigali',24);
-insert into item(id, name, weather) values(102,'Musanze',18);
-insert into item(id, name, weather) values(103,'Rubavu',20);
-insert into item(id, name, weather) values(104,'Nyagatare',28);
